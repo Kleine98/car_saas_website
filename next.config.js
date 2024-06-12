6985c44578fc29daa505c05134f051b3a6abb45e
kleine <ppyyde02@gmail.com>
2024-06-12 13:03:30 +0700

    Fixed Backend endpoint

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {}
+// next.config.js
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
-module.exports = nextConfig
+module.exports = {
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost:3001/:path*", // Proxy to Backend
+      },
+    ];
+  },
+  // Additional custom config for development server
+  webpackDevMiddleware: (config) => {
+    config.watchOptions = {
+      poll: 1000, // Check for changes every second
+      aggregateTimeout: 300, // Delay before rebuilding
+    };
+    return config;
+  },
+};
